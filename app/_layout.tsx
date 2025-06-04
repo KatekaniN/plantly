bf1292f220abf56e2d26f5b139c180877af3e88f
KatekaniN <knyamandi99@gmail.com>
2025-06-04 11:48:33 +0200

    push files to mac

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,6 @@
 import { Tabs } from "expo-router";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function Layout() {
     return (
@@ -6,10 +8,26 @@ export default function Layout() {
             screenOptions={{
                 sceneStyle: { paddingTop: 8, paddingBottom: 8 },
                 tabBarActiveTintColor: "#fa7acb",
+                tabBarInactiveTintColor: "#eee",
+                tabBarShowLabel: false,
             }}>
             <Tabs.Screen
                 name="index"
-                options={{ title: "Home" }}>
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ size, color }) => (
+                        <FontAwesome name="home" size={size} color={color} />
+                    )
+                }}>
+            </Tabs.Screen>
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: "Profile",
+                    tabBarIcon: ({ size, color }) => (
+                        <MaterialIcons name="account-circle" size={size} color={color} />
+                    )
+                }}>
             </Tabs.Screen>
         </Tabs>
     );
