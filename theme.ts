ad3c100014641011bbb231efefa103d610821fd5
KatekaniN <knyamandi99@gmail.com>
2025-06-05 10:14:32 +0200

    push files to mac

--- theme.ts
+++ theme.ts
@@ -4,4 +4,7 @@ export const theme = {
     colorWhite: "#ffffff",
     colorBlack: "#000000",
     colorGrey: "#948e8e",
+    colorBlue: "#2196F3",
+    colorPurple: "#9C27B0",
+    colorOrange: "#FF9800",
 }
