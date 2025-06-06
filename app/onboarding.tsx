558a13172e99888a503fbadb16f02cf3a905579c
KatekaniN <knyamandi99@gmail.com>
2025-06-06 12:28:50 +0200

    push files to mac

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -57,7 +57,7 @@ const styles = StyleSheet.create({
 
     },
     headerText: {
-        fontSize: 32,
+        fontSize: 40,
         fontWeight: "bold",
         top: 20,
         marginBottom: 20,
