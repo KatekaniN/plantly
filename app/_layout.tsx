9933f02b1cbb1a7360d00878bfee0551f584890f
KatekaniN <knyamandi99@gmail.com>
2025-06-11 15:40:47 +0200

    push

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,31 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Layout() {
     return (
-        <Stack>
-            <Stack.Screen
-                name="(tabs)"
-                options={{
-                    headerShown: false,
-                    animation: 'fade',
-                }}>
-            </Stack.Screen>
-            <Stack.Screen
-                name="onboarding"
-                options={{
-                    animation: 'fade',
-                    headerShown: false,
-                    title: 'Onboarding',
-                    presentation: 'modal',
-                }}>
-            </Stack.Screen>
-        </Stack>
+        <SafeAreaProvider>
+            <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+                <Stack>
+                    <Stack.Screen
+                        name="(tabs)"
+                        options={{
+                            headerShown: false,
+                            animation: 'fade',
+                        }}>
+                    </Stack.Screen>
+                    <Stack.Screen
+                        name="onboarding"
+                        options={{
+                            animation: 'fade',
+                            headerShown: false,
+                            title: 'Onboarding',
+                            presentation: 'modal',
+                        }}>
+                    </Stack.Screen>
+                </Stack >
+            </SafeAreaView>
+        </SafeAreaProvider >
     )
 }
--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -160,7 +160,7 @@ const styles = StyleSheet.create({
         marginBottom: -(windowHeight * 0.01),
     },
     title: {
-        fontSize: 28,
+        fontSize: 30,
         fontWeight: 'bold',
         color: "#fff",
         textAlign: 'center',
